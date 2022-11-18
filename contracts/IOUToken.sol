2675f54ae4f9f34e6b8543a64325796679cdf476
phongho01 <phong.ho@napaglobal.com>
2022-11-18 11:33:36 +0700

    restore burn function and add tests for IOUToken contract

--- contracts/IOUToken.sol
+++ contracts/IOUToken.sol
@@ -10,4 +10,13 @@ contract IOUToken is ERC20 {
     constructor(address reBakedDao_, uint256 totalSupply_, string memory name_, string memory symbol_) ERC20(name_, symbol_) {
         _mint(reBakedDao_, totalSupply_);
     }
+
+    /**
+     * @notice Burn owned token
+     * @param amount_ Amount of token want to burn
+     * Emit {Transfer}
+     */
+    function burn(uint256 amount_) external {
+        _burn(_msgSender(), amount_);
+    }
 }
