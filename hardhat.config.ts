df574e51d453ee62e4ef3ad1ba72d6242c8af08f
phongho01 <phong.ho@napaglobal.com>
2023-12-06 12:00:42 +0700

    update scripts to deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,11 @@ import { HardhatUserConfig } from "hardhat/config";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import "@nomiclabs/hardhat-ethers";
+
+// Verify and public source code on etherscan
+import "@nomiclabs/hardhat-etherscan";
+
 // All tools for hardhat and smart contracts
 import "@nomicfoundation/hardhat-toolbox";
 
@@ -12,6 +17,9 @@ import "@openzeppelin/hardhat-upgrades";
 // Show contract's size
 import "hardhat-contract-sizer";
 
+// reporter
+import "hardhat-gas-reporter";
+
 const config: HardhatUserConfig = {
 	defaultNetwork: "hardhat",
 	networks: {
@@ -20,52 +28,63 @@ const config: HardhatUserConfig = {
 		},
 		goerli: {
 			url: "https://eth-goerli.g.alchemy.com/v2/yiaZ5Hg5fTRH46ijJywVW1WF5ltv47xI",
-			accounts: [process.env.GOERLI_DEPLOY_ACCOUNT!],
+			accounts: [process.env.DEPLOY_ACCOUNT!],
 			chainId: 5,
 		},
 		polygonMumbai: {
 			url: "https://matic-mumbai.chainstacklabs.com",
-			accounts: [process.env.POLYGON_DEPLOY_ACCOUNT!],
+			accounts: [process.env.DEPLOY_ACCOUNT!],
 			chainId: 80001,
-		},
-		bscTestnet: {
-			url: "https://data-seed-prebsc-1-s3.binance.org:8545/",
-			accounts: [process.env.BSC_DEPLOY_ACCOUNT!],
-			chainId: 97,
 		},
 		mumbai: {
 			url: "https://polygon-mumbai.g.alchemy.com/v2/zSybnsuH3h_jqalkVmYv2Xbf-bsc3hlx",
-			accounts: [process.env.MUMBAI_DEPLOY_ACCOUNT as string]
+			accounts: [process.env.DEPLOY_ACCOUNT as string],
 		},
 		auroraTestnet: {
 			url: "https://testnet.aurora.dev",
-			accounts: [process.env.AURORA_TESTNET_DEPLOY_ACCOUNT!]
+			accounts: [process.env.DEPLOY_ACCOUNT!],
 		},
 		arbitrumGoerli: {
-			// url: "https://arb-goerli.g.alchemy.com/v2/HvsY1VKZgg_YEJHv_o9GAjjaLmmEO8Xl",
 			url: "https://goerli-rollup.arbitrum.io/rpc",
-			accounts: [process.env.ARBITRUM_GOERLI_DEPLOY_ACCOUNT!]
-		}
+			accounts: [process.env.DEPLOY_ACCOUNT!],
+		},
+		mainnet: {
+			url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+			accounts: [process.env.DEPLOY_ACCOUNT!],
+			chainId: 5,
+		},
+		polygon: {
+			url: "https://polygon-rpc.com/",
+			accounts: [process.env.DEPLOY_ACCOUNT as string],
+		},
+		aurora: {
+			url: "https://mainnet.aurora.dev",
+			accounts: [process.env.DEPLOY_ACCOUNT!],
+		},
+		arbitrumOne: {
+			url: "https://arb1.arbitrum.io/rpc",
+			accounts: [process.env.DEPLOY_ACCOUNT!],
+		},
 	},
 	etherscan: {
 		apiKey: {
-			goerli: process.env.GOERLI_SCAN_API_KEY!,
-			polygonMumbai: process.env.POLYGON_TEST_API_KEY!,
-			bscTestnet: process.env.BSC_TEST_API_KEY!,
-			auroraTestnet: process.env.AURORA_TEST_API_KEY!,
-			arbitrumGoerli: process.env.ARBITRUM_GOERLI_TEST_API_KEY!
+			goerli: process.env.SCAN_API_KEY!,
+			polygonMumbai: process.env.POLYGON_API_KEY!,
+			arbitrumGoerli: process.env.ARBITRUM_API_KEY!,
+			mainnet: process.env.SCAN_API_KEY!,
+			polygon: process.env.POLYGON_API_KEY!,
+			arbitrumOne: process.env.ARBITRUM_API_KEY!,
 		},
 		customChains: [
 			{
 				network: "arbitrumGoerli",
 				chainId: 421613,
 				urls: {
-					// apiURL: "https://arb-goerli.g.alchemy.com/v2/HvsY1VKZgg_YEJHv_o9GAjjaLmmEO8Xl",
 					apiURL: "https://goerli-rollup.arbitrum.io/rpc",
-					browserURL: "https://goerli.arbiscan.io/"
-				}
-			}
-		]
+					browserURL: "https://goerli.arbiscan.io/",
+				},
+			},
+		],
 	},
 	solidity: {
 		compilers: [
